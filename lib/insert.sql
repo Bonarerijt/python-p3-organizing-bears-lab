ee3c92664b8dfc07accaa85f24632b78f0c5a51c
Bonarerijt <prudyjj4@gmail.com>
2025-11-30 21:14:32 +0300

    Created and manipulated tables with SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 10, "M", "blue", "sweet", 1 ),
+("Rowdy", 6, "M", "yellow", "friendly", 1),
+("Tabitha", 15, "F","orange", "calm", 1),
+("Sergeant Brown", 20, "M", "brown", "teritorial", 0),
+("Melissa", 12, "F", "pink", "fiesty", 1), 
+("Grinch", 10, "M", "green", "angry", 1),
+("Wendy", 7, "F", "purple", "short_tempered", 1),
+(NULL, 8, "M", "red", "agitated", 0);
